2744462c961580c89214bf344d0847cdf7787545
Audric de Schaetzen <audric.deschaetzen+github@gmail.com>
2015-10-14 17:41:17 +0200

    hands are typing words

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,17 +6,27 @@ public class BulletScript : MonoBehaviour {
 	//public GameObject splatter;
 	//public GameObject splatter2;
 	public GameObject Paint;
-
+	public AudioClip ImpactSound;
+	public int Deathcount = 5;
+	private float startTime;
 	// Use this for initialization
 	void Start () {
-		
+		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Time.time - startTime >= Deathcount)
+		{
+			Destroy(gameObject);
+		}
 	}
-	
+
+	void OnDestroy()
+	{
+		AudioSource.PlayClipAtPoint(ImpactSound, transform.position);
+	}
+
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
